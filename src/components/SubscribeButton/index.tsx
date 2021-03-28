1de1a14d51f3490b356262ebd7dec4be2add6b75
André <andrevictor50@gmail.com>
2021-03-28 10:10:28 -0300

    Creating redirect for Stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,6 +1,8 @@
 import { useCallback } from 'react';
 import { signIn, useSession } from 'next-auth/client';
 import styles from './styles.module.scss';
+import { api } from '../../services/api';
+import { getStripeJs } from '../../services/stripe-js';
 
 export type SubscribeButtonProps = {
   priceId: string;
@@ -9,17 +11,33 @@ export type SubscribeButtonProps = {
 export function SubscribeButton({ priceId }: SubscribeButtonProps) {
   const [ session ] = useSession();
 
-  const handleSubscribe = useCallback(() => {
+  const handleSubscribe = useCallback(async () => {
     if(!session) {
       signIn('github');
       return;
     }
 
-    
+    try {
+      const response = await api.post('/subscribe');
+
+      const { sessionId } = response.data;
+
+      const stripe = await getStripeJs();
+
+      await stripe.redirectToCheckout({
+        sessionId,
+      });
+    } catch(err) {
+      alert(err.message);
+    }
   }, [session, signIn]);
 
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button
+      type="button"
+      className={styles.subscribeButton}
+      onClick={handleSubscribe}
+    >
       Subscribe now
     </button>
   );
