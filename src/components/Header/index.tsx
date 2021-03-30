29d4ddc74a228bb8fb0fd0ea1f51d85171f54c1e
André <andrevictor50@gmail.com>
2021-03-30 07:04:44 -0300

    Adding Header navigation

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,5 +1,6 @@
 import styles from './styles.module.scss';
 import { Button } from '../Button';
+import { ActiveLink } from '../ActiveLink';
 
 export function Header() {
   return (
@@ -7,10 +8,13 @@ export function Header() {
       <div  className={styles.headerContent}>
         <img src="/images/logo.svg" alt="ignews logo" />
         <nav>
-          <a href="/" className={styles.active}>Home</a>
-          <a href="/posts">Posts</a>
+          <ActiveLink activeClassName={styles.active} href="/">
+            <a>Home</a>        
+          </ActiveLink>
+          <ActiveLink activeClassName={styles.active} href="/posts" prefetch>        
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
-
         <Button />
       </div>
     </header>
